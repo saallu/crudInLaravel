8e4125db0612f30032ea89efb56c3d0af62e5fef
saallu <salmanarrahman@gmail.com>
2020-06-24 23:03:26 +0600

    first commit

---
+++ resources/views/students/edit.blade.php
@@ -0,0 +1,24 @@
+@extends('master')
+@section('content')
+
+    <div class="row">
+        <div class="col-md-12">
+            <br />
+            <h3>Edit Record</h3>
+            <br />
+            <form method="post" action="{{action('StudentController@update',$id)}}">
+                {{csrf_field()}}
+                <input type="hidden" name="_method" value="PATCH" />
+                <div class="form-group">
+                    <input type="text" name="first_name" class="form-control" value="{{$student->first_name}}" placeholder="Enter First Name" />
+                </div>
+                <div class="form-group">
+                    <input type="text" name="last_name" class="form-control" value="{{$student->last_name}}" placeholder="Enter Last Name" />
+                </div>
+                <div class="form-group">
+                    <input type="submit" class="btn btn-primary" value="Edit" />
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
